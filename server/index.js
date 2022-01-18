430be38736e6a63ac11a1ea0c8928bd9166e48d8
zak <zakwarsamee@gmail.com>
2022-01-18 10:33:24 -0500

    fix bug on index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const inventoryRoute = require("./routes/inventory");
-const cors = require('cors');
+const cors = require("cors");
 
 dotenv.config();
 
@@ -13,8 +13,6 @@ mongoose
   .then(() => console.log("db connected successfully"))
   .catch((err) => console.log(err));
 
-  
-
 // ======== Middleware ========
 
 //json parser middleware
@@ -23,7 +21,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/inventory", inventoryRoute);
 
-app.get('/', (req, res) => { res.send('Hello from Express!')
+app.get("/", (req, res) => res.send("Hello from Express!"));
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("server running on 5000");
